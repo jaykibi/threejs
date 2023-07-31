0d2648dcc86a74c8dd92d9b176eb06420d9f6ef7
Raquibbi Muhammaddi <raquibbi@gmail.com>
2023-07-31 16:02:28 -0500

    lesson 10 start

--- 09-geometries/src/script.js
+++ 09-geometries/src/script.js
@@ -8,8 +8,25 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 // Object
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshPhongMaterial({});
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 1, 1, 1);
+
+const geometry = new THREE.BufferGeometry();
+
+const count = 500;
+const positionsArray = new Float32Array(count * 3 * 3);
+
+for (let i = 0; i < count * 3 * 3; i++) {
+    positionsArray[i] = Math.random();
+}
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+geometry.setAttribute("position", positionsAttribute);
+
+const material = new THREE.MeshBasicMaterial({
+    color: "red",
+    wireframe: true,
+});
+
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
@@ -45,7 +62,7 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     100
 );
-camera.position.z = 50;
+camera.position.z = 5;
 scene.add(camera);
 
 // Controls
